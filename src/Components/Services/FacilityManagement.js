5c9ccb98239b4de8050e82c94fa549126b67ea97
mahalakshmiyml <lakshmiyml@gmail.com>
2022-03-15 13:12:11 +0530

    modified financial to logistics

--- src/Components/Services/FacilityManagement.js
+++ src/Components/Services/FacilityManagement.js
@@ -326,23 +326,15 @@ const FacilityManagement = () => {
         </Container>
       </div>
 
-      <div id="financial" className="pt-3 pb-3 py-md-5 bg-light">
+      <div id="logistics" className="pt-3 pb-3 py-md-5 bg-light">
         <Container>
           <Row>
             <h4 className="text-primary text-center py-3">
-              BANKING AND FINANCIAL INSTITUTIONS{" "}
+            LOGISTICS & WAREHOUSE MANAGEMENT
             </h4>
 
             <p>
-              Security personnel is to ensure safety of the bank in all
-              respects. A bank security guard formulates strategies and suggests
-              the use of new security devices and facilities to the bank
-              management to increase the security of the bank. Monitoring the
-              entry and exit of the bank, ensuring safety from thefts and other
-              miscreant activities, monitoring customer activity and behavior in
-              the bank to identify suspicious activity, Monitor existing
-              security instruments like computers, close circuit cameras and
-              others.
+            Access control refers to any system or mechanism that allows you to control entry and movement of people throughout you premises, bringing total security and safeguarding property. CFS provides exhaustive physical access control systems and solutions to help you secure and protect your premises.
             </p>
           </Row>
           <Row className="g-3 justify-content-center">
